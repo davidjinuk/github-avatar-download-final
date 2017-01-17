237ff59e0cb45820ef256b07731b6d70726aa046
David Kim <davidjinuk90@gmail.com>
2017-01-17 00:57:22 +0000

    invoked the function using hard coded values. within the function it prints repoowner and reponame along with cb as null for both values

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  console.log(repoOwner);
+  console.log(repoName);
+  cb(null, null);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
