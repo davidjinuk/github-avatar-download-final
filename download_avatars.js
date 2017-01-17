c310559a7fba545d9203b8ef5633735a1d36b0c1
David Kim <davidjinuk90@gmail.com>
2017-01-17 02:39:56 +0000

    created variable results to hold JSON body. printed the login and avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -8,10 +8,27 @@ var GITHUB_TOKEN = '908e91f9130a91e1e46d2bebc080511c9c3560a1';
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // console.log(repoOwner);
-  // console.log(repoName);
-  // cb(null, null);
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request(options, function(err, response, body) {
+    if (err) throw err;
+    console.log('Response Status Code:', response.statusCode);
+
+    var results = JSON.parse(body);
+    results.forEach(function(value, index){
+      var login = value.login;
+      var avatar = value.avatar_url;
+
+      console.log(login);
+      console.log(avatar);
+    });
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
