feaed4111521469c47620d0fa81dc10d87974189
David Kim <davidjinuk90@gmail.com>
2017-01-17 04:14:00 +0000

    connected the two functions and created a new directory to store the images

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,18 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = 'davidjinuk';
 var GITHUB_TOKEN = '908e91f9130a91e1e46d2bebc080511c9c3560a1';
+
+var mkdir = function () {
+  try {
+    fs.mkdir('avatars');
+  } catch (e) {
+    if (e.code != 'EEXIST') throw e;
+  }
+}();
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
@@ -22,38 +31,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     var results = JSON.parse(body);
     results.forEach(function(value, index){
-      var login = value.login;
-      var avatar = value.avatar_url;
 
-      console.log(login);
-      console.log(avatar);
+    var login = 'avatars/' + value.login + '.jpg';
+    var avatar = value.avatar_url;
+
+    downloadImageByURL(avatar, login);
     });
   });
 }
 
 function downloadImageByURL(url, filePath) {
-  // ...
-  var fs = require('fs');
   request.get(url)
          .on('error', function (err) {
            throw err;
          })
          .on('response', function (response) {
-           console.log('Response Status Code: ', response.statusCode);
-           console.log('Response Status Message: ', response.statusMessage);
            console.log('Content Type: ', response.headers['content-type']);
-           console.log('Downloading image...');
          })
          .pipe(fs.createWriteStream(filePath))
-
-         .on('finish', function () {
-            console.log('Download Complete');
-         });
 }
-downloadImageByURL('https://avatars.githubusercontent.com/u/1615?v=3', './jeresig.jpg');
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-
