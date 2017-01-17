9431f7f3c5da9c19f2ebf8c4d6f065cc9b373f3c
David Kim <davidjinuk90@gmail.com>
2017-01-17 02:09:22 +0000

    added variables for github user and token. printed the requestURL and confirmed it works.

--- download_avatars.js
+++ download_avatars.js
@@ -2,14 +2,20 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'davidjinuk';
+var GITHUB_TOKEN = '908e91f9130a91e1e46d2bebc080511c9c3560a1';
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  console.log(repoOwner);
-  console.log(repoName);
-  cb(null, null);
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+  // console.log(repoOwner);
+  // console.log(repoName);
+  // cb(null, null);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
